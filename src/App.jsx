510b8717ef44814bbb5117fc602e643278b2cf54
Dharamveer Singh <dveersingh000@gmail.com>
2024-12-23 06:37:51 +0530

    building landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-
+import LandingPage from "./pages/LandingPage";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import SignIn from "./pages/SignIn";
+// import CreateFormBot from "./pages/CreateFormBot";
 
 function App() {
 
   return (
     <>
-      <div>
-        FORM BUILDER APP
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
